567a7c6c3152601e4796160a789af3a5a1e33d23
huang <huang_jiangling@163.com>
2019-09-29 14:53:20 +0800

    项目初始化；

---
+++ src/main/java/com/huang/practice/guava/chapter01/test.java
@@ -0,0 +1,10 @@
+package com.huang.practice.guava.chapter01;
+
+/**
+ * test -
+ *
+ * @author: huang_jiangling
+ * @date: 2019-09-29
+ **/
+public class test {
+}
---
+++ src/main/java/com/huang/practice/guava/chapter02/test2.java
@@ -0,0 +1,10 @@
+package com.huang.practice.guava.chapter02;
+
+/**
+ * test2 -
+ *
+ * @author: huang_jiangling
+ * @date: 2019-09-29
+ **/
+public class test2 {
+}
